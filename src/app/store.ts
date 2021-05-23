ea4ec96b1d228185c0e6bbfd8fee0314eab5e00e
Chayanon Thongpila <nutchy.th@gmail.com>
2021-05-24 00:32:35 +0700

    feat: make store persisted

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,44 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
+import { createStore, configureStore, ThunkAction, Action, combineReducers, applyMiddleware } from '@reduxjs/toolkit'
 
 import counterReducer from '../features/counter/counterSlice'
 
+const combinedReducer = combineReducers({
+  counter: counterReducer
+})
+
 export function makeStore() {
-  return configureStore({
-    reducer: { counter: counterReducer },
-  })
+  const isServer = typeof window === 'undefined'
+  if (isServer) {
+    const store = configureStore({
+      reducer: combinedReducer
+    })
+
+    return { store, persistor: null }
+  }
+  else {
+
+    //If it's on client side, create a store which will persist
+    const { persistStore, persistReducer } = require("redux-persist")
+    const storage = require("redux-persist/lib/storage").default
+
+    const persistConfig = {
+      key: "root",
+      whitelist: ["counter"], // only counter will be persisted, add other reducers if needed
+      storage, // if needed, use a safer storage
+    }
+
+    const persistedReducer = persistReducer(persistConfig, combinedReducer) // Create a new reducer with our existing reducer
+
+    const store = configureStore({
+      reducer: persistedReducer
+    })
+
+    const persistor = persistStore(store)
+    return { store, persistor }
+  }
 }
 
-const store = makeStore()
+const { store, persistor } = makeStore()
 
 export type AppState = ReturnType<typeof store.getState>
 
@@ -22,3 +52,4 @@ export type AppThunk<ReturnType = void> = ThunkAction<
 >
 
 export default store
+export { persistor }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,19 @@
 import '../styles/globals.css'
 
 import { Provider } from 'react-redux'
+
+// @ts-ignore
+import { PersistGate } from 'redux-persist/integration/react'
 import type { AppProps } from 'next/app'
 
-import store from '../app/store'
+import store, { persistor } from '../app/store'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
     </Provider>
   )
 }
